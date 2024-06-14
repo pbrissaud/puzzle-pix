90508af44587fbb147b3247a6d2d7888e8e640a2
Paul Brissaud <pbrissaud.pro@gmail.com>
2024-06-15 01:44:20 +0200

    feat: tailwind + shadcn/ui setup

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,8 +3,4 @@ const path = require("path");
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
-  output: "standalone",
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, "../../"),
-  },
 };
